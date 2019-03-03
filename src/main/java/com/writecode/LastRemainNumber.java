42f1e3ed885311ec543859e46c79fda5cf4054fc
wickedvalley <520mzydssw>
2019-03-03 21:03:06 +0800

    完善约瑟夫环问题

--- src/main/java/com/writecode/LastRemainNumber.java
+++ src/main/java/com/writecode/LastRemainNumber.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class LastRemainNumber {
 
     /**
+     * 约瑟夫环问题
      *n=6 ,  m=2
      * 0 1 2 3 4 5
      *   1
@@ -13,27 +14,30 @@ public class LastRemainNumber {
      *
      * 1 4
      * */
-    private static int lastRemainNumber(int n ,int m){
-        List<Integer> list  = new ArrayList<Integer>();
-        for(int i=0;i<n;i++){
+
+    public static int LastRemaining_Solution(int n, int m) {
+        if(n==0){
+            return -1;
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=1;i<=n;i++){
             list.add(i);
         }
-        int count = 0;
-        int index =-1;
-        while (list.size()!=1){
+        int index=-1;
+        int len =0;
+        while (list.size()>1){
             index=(index+1)%list.size();
-            count++;
-            if (count == m){
-                System.out.println(list.get(index));
+            len++;
+            if(len==m){
                 list.remove(index);
                 index=(index-1)%list.size();
-                count=0;
+                len=0;
             }
         }
         return list.get(0);
     }
 
     public static void main(String[] args) {
-        lastRemainNumber(6,2);
+        System.out.println(LastRemaining_Solution(50,3));
     }
 }
